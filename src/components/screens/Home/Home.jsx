e01d115b40dd9c7f2fa58bd59e843dd3702e0acf
frostyx-vk <vk.xam@yandex.ru>
2023-01-11 16:04:52 +0600

    changes: add css

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from "./Home.module.css"
 
 const Home = () => {
     return (
-        <div>
+        <div className={classes.page}>
             Home
         </div>
     )
